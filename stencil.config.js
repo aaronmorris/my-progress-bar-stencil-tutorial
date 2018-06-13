d10a9724d1485ff83325675035a0b945c506c1ff
Aaron Morris <aaron.morris@ncino.com>
2018-06-13 10:06:55 -0400

    Initial Installation and Configuration

---
+++ stencil.config.js
@@ -0,0 +1,6 @@
+exports.config = {
+    namespace: 'my-progress-bar-stencil-tutorial',
+    generateDistribution: true,
+    generateWWW: false,
+    bundles: [{ components: ['my-progress-bar']}]
+};
